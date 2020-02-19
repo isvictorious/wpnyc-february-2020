e63afd3ab97c28375745b89cc574d558243857a4
isvictorious <me@isvictorious.com>
2020-02-18 19:22:31 -0500

    added child theme and functions plugin

---
+++ wp-content/plugins/wpnyc-functions/types/type-service.php
@@ -0,0 +1,66 @@
+<?php
+
+// Register Custom Post Type
+function wpnyc_services_post_type()
+{
+
+	$labels = array(
+		'name'                  => _x('Services', 'Post Type General Name', 'sai_functions'),
+		'singular_name'         => _x('Service', 'Post Type Singular Name', 'sai_functions'),
+		'menu_name'             => __('Services', 'sai_functions'),
+		'name_admin_bar'        => __('Service', 'sai_functions'),
+		'archives'              => __('Service Archives', 'sai_functions'),
+		'attributes'            => __('Service Attributes', 'sai_functions'),
+		'parent_item_colon'     => __('Parent Service:', 'sai_functions'),
+		'all_items'             => __('All Services', 'sai_functions'),
+		'add_new_item'          => __('Add New Service', 'sai_functions'),
+		'add_new'               => __('Add New', 'sai_functions'),
+		'new_item'              => __('New Service', 'sai_functions'),
+		'edit_item'             => __('Edit Service', 'sai_functions'),
+		'update_item'           => __('Update Service', 'sai_functions'),
+		'view_item'             => __('View Service', 'sai_functions'),
+		'view_items'            => __('View Services', 'sai_functions'),
+		'search_items'          => __('Search services', 'sai_functions'),
+		'not_found'             => __('No services found', 'sai_functions'),
+		'not_found_in_trash'    => __('No services found in Trash', 'sai_functions'),
+		'featured_image'        => __('Featured Image', 'sai_functions'),
+		'set_featured_image'    => __('Set featured image', 'sai_functions'),
+		'remove_featured_image' => __('Remove featured image', 'sai_functions'),
+		'use_featured_image'    => __('Use as featured image', 'sai_functions'),
+		'insert_into_item'      => __('Insert into item', 'sai_functions'),
+		'uploaded_to_this_item' => __('Uploaded to this item', 'sai_functions'),
+		'items_list'            => __('Services list', 'sai_functions'),
+		'items_list_navigation' => __('Services list navigation', 'sai_functions'),
+		'filter_items_list'     => __('Filter resources list', 'sai_functions'),
+	);
+	$rewrite = array(
+		'slug'                  => 'services',
+		'with_front'            => false,
+		'pages'                 => true,
+		'feeds'                 => true,
+	);
+	$args = array(
+		'label'                 => __('Service', 'sai_functions'),
+		'description'           => __('Service information pages.', 'sai_functions'),
+		'labels'                => $labels,
+		'supports'              => array('title', 'editor', 'thumbnail', 'page-attributes'),
+		'taxonomies'            => array(),
+		'hierarchical'          => true,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 12,
+		'menu_icon'             => 'dashicons-businesswoman',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'rewrite'               => $rewrite,
+		'capability_type'       => 'page',
+		'show_in_rest' => true,
+	);
+	register_post_type('service', $args);
+}
+add_action('init', 'wpnyc_services_post_type', 0);
---
+++ wp-content/plugins/wpnyc-functions/types/type-testimonial.php
@@ -0,0 +1,173 @@
+<?php
+
+// Register Custom Post Type
+function wpnyc_testimonial_post_type()
+{
+
+	$labels = array(
+		'name'                  => _x('Testimonials', 'Post Type General Name', 'sai_functions'),
+		'singular_name'         => _x('Testimonial', 'Post Type Singular Name', 'sai_functions'),
+		'menu_name'             => __('Testimonials', 'sai_functions'),
+		'name_admin_bar'        => __('Testimonial', 'sai_functions'),
+		'archives'              => __('Testimonial Archives', 'sai_functions'),
+		'attributes'            => __('Testimonial Attributes', 'sai_functions'),
+		'parent_item_colon'     => __('Parent Testimonial:', 'sai_functions'),
+		'all_items'             => __('All Testimonials', 'sai_functions'),
+		'add_new_item'          => __('Add New Testimonial', 'sai_functions'),
+		'add_new'               => __('Add New', 'sai_functions'),
+		'new_item'              => __('New Testimonial', 'sai_functions'),
+		'edit_item'             => __('Edit Testimonial', 'sai_functions'),
+		'update_item'           => __('Update Testimonial', 'sai_functions'),
+		'view_item'             => __('View Testimonial', 'sai_functions'),
+		'view_items'            => __('View Testimonials', 'sai_functions'),
+		'search_items'          => __('Search Testimonial', 'sai_functions'),
+		'not_found'             => __('Not found', 'sai_functions'),
+		'not_found_in_trash'    => __('Not found in Trash', 'sai_functions'),
+		'featured_image'        => __('Featured Image', 'sai_functions'),
+		'set_featured_image'    => __('Set featured image', 'sai_functions'),
+		'remove_featured_image' => __('Remove featured image', 'sai_functions'),
+		'use_featured_image'    => __('Use as featured image', 'sai_functions'),
+		'insert_into_item'      => __('Insert into Testimonial', 'sai_functions'),
+		'uploaded_to_this_item' => __('Uploaded to this Testimonial', 'sai_functions'),
+		'items_list'            => __('Testimonials list', 'sai_functions'),
+		'items_list_navigation' => __('Testimonials list navigation', 'sai_functions'),
+		'filter_items_list'     => __('Filter Testimonials list', 'sai_functions'),
+	);
+	$args = array(
+		'label'                 => __('Testimonial', 'sai_functions'),
+		'description'           => __('Testimonial information page.', 'sai_functions'),
+		'labels'                => $labels,
+		'supports'              => array('title', 'thumbnail'),
+		'taxonomies'            => array(),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 12,
+		'menu_icon'             => 'dashicons-awards',
+		'show_in_admin_bar'     => false,
+		'show_in_nav_menus'     => false,
+		'can_export'            => true,
+		'has_archive'           => false,
+		'exclude_from_search'   => true,
+		'publicly_queryable'    => false,
+		'capability_type'       => 'post',
+	);
+	register_post_type('testimonial', $args);
+}
+add_action('init', 'wpnyc_testimonial_post_type', 0);
+
+
+
+
+
+/**
+ * Testimonial CPT updates messages.
+ *
+ * @param array $messages Existing post update messages.
+ *
+ * @return array Amended testimonial CPT notices
+ * 
+ * @link https://www.sitepoint.com/wordpress-custom-post-types-notices-taxonomies/
+ */
+function wpnyc_testimonial_notices( $messages ) {
+    $post             = get_post();
+    $post_type        = get_post_type( $post );
+    $post_type_object = get_post_type_object( $post_type );
+
+    $messages['testimonial'] = array(
+        0  => '', // Unused. Messages start at index 1.
+        1  => __( 'Testimonial updated.', 'textdomain' ),
+        2  => __( 'Custom field updated.', 'textdomain' ),
+        3  => __( 'Custom field deleted.', 'textdomain' ),
+        4  => __( 'Testimonial updated.', 'textdomain' ),
+        5  => isset( $_GET['revision'] ) ? sprintf( __( 'Testimonial restored to revision from %s', 'textdomain' ), wp_post_revision_title( (int) $_GET['revision'], false ) ) : false,
+        6  => __( 'Testimonial published.', 'textdomain' ),
+        7  => __( 'Testimonial saved.', 'textdomain' ),
+        8  => __( 'Testimonial submitted.', 'textdomain' ),
+        9  => sprintf(
+            __( 'Testimonial scheduled for: <strong>%1$s</strong>.', 'textdomain' ),
+            date_i18n( __( 'M j, Y @ G:i', 'textdomain' ), strtotime( $post->post_date ) )
+        ),
+        10 => __( 'Testimonial draft updated.', 'textdomain' )
+    );
+
+    if ( $post_type_object->publicly_queryable ) {
+        $permalink = get_permalink( $post->ID );
+
+        $view_link = sprintf( ' <a href="%s">%s</a>', esc_url( $permalink ), __( 'View testimonial', 'textdomain' ) );
+        $messages[ $post_type ][1] .= $view_link;
+        $messages[ $post_type ][6] .= $view_link;
+        $messages[ $post_type ][9] .= $view_link;
+
+        $preview_permalink = add_query_arg( 'preview', 'true', $permalink );
+        $preview_link      = sprintf( ' <a target="_blank" href="%s">%s</a>', esc_url( $preview_permalink ), __( 'Preview testimonial', 'textdomain' ) );
+        $messages[ $post_type ][8] .= $preview_link;
+        $messages[ $post_type ][10] .= $preview_link;
+    }
+
+    return $messages;
+}
+add_filter( 'post_updated_messages', 'wpnyc_testimonial_notices' );
+
+
+/**
+ * Admin Columns Pro settings for Testimonials
+ *
+ * @return void
+ * 
+ * @link https://www.admincolumns.com/documentation/guides/how-to-use-php-exported-column-settings/
+ */
+function ac_custom_column_settings_a7d28d77() {
+
+	ac_register_columns( 'testimonial', array(
+		array(
+			'columns' => array(
+				'title' => array(
+					'type' => 'title',
+					'label' => 'Title',
+					'width' => '',
+					'width_unit' => '%',
+					'edit' => 'on',
+					'sort' => 'on',
+					'name' => 'title',
+					'label_type' => '',
+					'bulk-editing' => '',
+					'export' => '',
+					'search' => ''
+				),
+				'5e3d90cfb4e03' => array(
+					'type' => 'column-featured_image',
+					'label' => 'Featured Image',
+					'width' => '',
+					'width_unit' => '%',
+					'featured_image_display' => 'image',
+					'image_size' => 'cpac-custom',
+					'image_size_w' => '60',
+					'image_size_h' => '60',
+					'edit' => 'on',
+					'sort' => 'on',
+					'filter' => 'off',
+					'filter_label' => '',
+					'name' => '5e3d90cfb4e03',
+					'label_type' => '',
+					'bulk-editing' => '',
+					'export' => '',
+					'search' => ''
+				),
+				'5e3d90cfb5ddc' => array(
+					'type' => 'column-actions',
+					'label' => 'Actions',
+					'width' => '',
+					'width_unit' => '%',
+					'use_icons' => '',
+					'name' => '5e3d90cfb5ddc',
+					'label_type' => '',
+					'export' => ''
+				)
+			),
+			
+		)
+	) );
+}
+add_action( 'ac/ready', 'ac_custom_column_settings_a7d28d77' );
