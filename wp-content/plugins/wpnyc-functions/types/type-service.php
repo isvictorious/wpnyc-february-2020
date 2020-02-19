e1ccfe069f40c956afc00e9c1843e302158f9fef
isvictorious <me@isvictorious.com>
2020-02-18 19:47:30 -0500

    added taxonomies

--- wp-content/plugins/wpnyc-functions/types/type-service.php
+++ wp-content/plugins/wpnyc-functions/types/type-service.php
@@ -64,3 +64,43 @@ function wpnyc_services_post_type()
 	register_post_type('service', $args);
 }
 add_action('init', 'wpnyc_services_post_type', 0);
+
+
+// Register Custom Taxonomy
+function region_taxonomy() {
+
+	$labels = array(
+		'name'                       => _x( 'Regions', 'Taxonomy General Name', 'text_domain' ),
+		'singular_name'              => _x( 'Region', 'Taxonomy Singular Name', 'text_domain' ),
+		'menu_name'                  => __( 'Region', 'text_domain' ),
+		'all_items'                  => __( 'All Regions', 'text_domain' ),
+		'parent_item'                => __( 'Parent Region', 'text_domain' ),
+		'parent_item_colon'          => __( 'Parent Region:', 'text_domain' ),
+		'new_item_name'              => __( 'New Region Name', 'text_domain' ),
+		'add_new_item'               => __( 'Add New Region', 'text_domain' ),
+		'edit_item'                  => __( 'Edit Region', 'text_domain' ),
+		'update_item'                => __( 'Update Region', 'text_domain' ),
+		'view_item'                  => __( 'View Item', 'text_domain' ),
+		'separate_items_with_commas' => __( 'Separate regions with commas', 'text_domain' ),
+		'add_or_remove_items'        => __( 'Add or remove regions', 'text_domain' ),
+		'choose_from_most_used'      => __( 'Choose from the most used regions', 'text_domain' ),
+		'popular_items'              => __( 'Popular Items', 'text_domain' ),
+		'search_items'               => __( 'Search regions', 'text_domain' ),
+		'not_found'                  => __( 'Not Found', 'text_domain' ),
+		'no_terms'                   => __( 'No items', 'text_domain' ),
+		'items_list'                 => __( 'Items list', 'text_domain' ),
+		'items_list_navigation'      => __( 'Items list navigation', 'text_domain' ),
+	);
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => true,
+		'public'                     => true,
+		'show_ui'                    => true,
+		'show_admin_column'          => true,
+		'show_in_nav_menus'          => true,
+		'show_tagcloud'              => true,
+	);
+	register_taxonomy( 'region', array( 'service' ), $args );
+
+}
+add_action( 'init', 'region_taxonomy', 0 );
